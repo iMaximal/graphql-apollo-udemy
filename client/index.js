5b2f650bbc058162c6376832441c6d08f4a5f4cd
Maximal <maxim@infofree.ru>
2018-02-15 15:22:26 +0500

    change ApolloProvider to 2.0.4

--- client/index.js
+++ client/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// frontend rendering library agnostic :-)
+// get data from server and store it locally
 import ApolloClient from 'apollo-client'
+// provides an integration between react and out server side
 import { ApolloProvider } from 'react-apollo'
+import SongList from './components/SongList/SongList'
 
 // path: /graphql by default
 const client = new ApolloClient({})
 
 const Root = () => {
   return (
-    <ApolloProvider store={} client={client} immutable={}>
-      <div>Lyrical</div>
+    <ApolloProvider client={client}>
+      <SongList />
     </ApolloProvider>
   )
 }
