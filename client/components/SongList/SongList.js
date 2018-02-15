a78c4b8538b81f4e083068bbdb6794a561505c58
Maximal <maxim@infofree.ru>
2018-02-15 17:42:23 +0500

    add view: songs

--- client/components/SongList/SongList.js
+++ client/components/SongList/SongList.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+
 // a helper to allow us to write queries inside of a component
 import gql from 'graphql-tag'
 
+// helper library, glue layer between React and the Apollo data source
+// to help us bond a component with an actual query
+import { graphql } from 'react-apollo'
+
 class SongList extends Component {
+
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      )
+    })
+   }
+
   render() {
+
+    if(this.props.data.loading) {
+      return <div>Loading...</div>
+    }
+
     return (
-      <div>
-        SongList
-      </div>
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul>
       )
   }
 }
@@ -15,10 +36,11 @@ class SongList extends Component {
 const query = gql`
   {
     songs {
+        id
         title
     }
   }
 `
 
 
-export default  SongList
+export default graphql(query)(SongList)
