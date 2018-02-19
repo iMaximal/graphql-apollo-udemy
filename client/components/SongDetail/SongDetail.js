234e2093f13664f5db790479ab21ddc675b66295
Maximal <maxim@infofree.ru>
2018-02-19 23:39:42 +0500

    add song title, query interceptor in SongDetail

--- client/components/SongDetail/SongDetail.js
+++ client/components/SongDetail/SongDetail.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import SongIndex from '../../queries/SongIndex'
 
 class SongDetail extends Component {
   render() {
+    const { song } = this.props.data
+
+    if (!song) { return <div>Loading...</div> }
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <h3>{song.title}</h3>
       </div>
     )
   }
 }
 
-export default SongDetail
+// the props first go into the GraphQL helper
+// and then the GraphQL helper passes them along to the SongDetail component
+export default graphql(SongIndex, {
+  options: (props) => ({ variables: { id: props.match.params.id } }),
+})(SongDetail)
